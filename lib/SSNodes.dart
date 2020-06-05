355707c65b3ea26c3b31fc833bdb90f3e6b843c7
Yixi <lyh1112@gmail.com>
2020-06-06 00:17:35 +0800

    add a grid view for ss nodes

--- lib/SSNodes.dart
+++ lib/SSNodes.dart
@@ -10,13 +10,57 @@ class SSNodes extends StatefulWidget {
   createState() => new _SSNodesState();
 }
 
+//class _SSNodesState extends State<SSNodes> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      child: Column(
+//        children: widget.nodes.map((node) => Text(node.name)).toList()
+//      )
+//    );
+//  }
+//}
+
 class _SSNodesState extends State<SSNodes> {
   @override
   Widget build(BuildContext context) {
+    return Flexible(
+        child: Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 1.2),
+        itemCount: widget.nodes.length,
+        itemBuilder: (context, index) {
+          return SSNode(node: widget.nodes[index]);
+        },
+      ),
+    ));
+  }
+}
+
+class SSNode extends StatelessWidget {
+  SSNode({this.node});
+
+  final SSConfigNode node;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: widget.nodes.map((node) => Text(node.name)).toList()
-      )
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: [
+            BoxShadow(
+                color: Color.fromARGB(25, 0, 0, 0),
+                offset: Offset(0, 2),
+                blurRadius: (6))
+          ]),
+      child: Text(node.name),
     );
   }
 }
